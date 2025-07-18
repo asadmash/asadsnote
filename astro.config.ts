65f873e531d57da60d367e39e2ce5f98d39984ef
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-18 22:00:03 +0600

    initial commit

--- astro.config.ts
+++ astro.config.ts
@@ -29,7 +29,7 @@ import rehypePostRaw from './plugins/rehype/rehype-post-raw';
 
 // https://astro.build/config
 const config: AstroUserConfig = {
-  site: 'https://suborbit.net/',
+  site: 'https://asadsnote.pages.dev/',
   // base: 'blog',
   output: 'static',
   integrations: [
