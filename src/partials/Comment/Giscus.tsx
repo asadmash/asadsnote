f7ecb79f20c1272c1bb202b8ee6977b6da137760
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-19 15:12:27 +0600

    giscus theme fix

--- src/partials/Comment/Giscus.tsx
+++ src/partials/Comment/Giscus.tsx
@@ -1,32 +1,66 @@
+// import GiscusComponent from '@giscus/react';
+// import type { GiscusProps as GiscusComponentProps } from '@giscus/react';
+// import useTheme from '@/hooks/useTheme';
+// import { BASE_URL } from '@/constants';
+// export interface GiscusOptions extends Omit<GiscusComponentProps, 'id'> {}
+
+// import LightTheme from './giscus-tailwind-light.css?url';
+// import DarkTheme from './giscus-tailwind-dark.css?url';
+
+// export default function Giscus(props: GiscusOptions) {
+//   const { colorMode } = useTheme();
+//   const baseUrl = window.location.origin + BASE_URL;
+//   return (
+//     <GiscusComponent
+//       id="comments"
+//       // theme={colorMode === 'dark' ? new URL(DarkTheme, baseUrl).toString() : new URL(LightTheme, baseUrl).toString()}
+//       // {...props}
+//        repo="asadmash/asadsnote"
+//         repoId="R_kgDOPOxLNA"
+//         category="General"
+//         categoryId="DIC_kwDOPOxLNM4CtJgq"
+//         mapping="pathname"
+//         strict="0"
+//         reactionsEnabled="1"
+//         emitMetadata="0"
+//         inputPosition="bottom"
+//         theme="preferred_color_scheme"
+//         lang="en"
+       
+//     />
+//   )
+// }
+
+
 import GiscusComponent from '@giscus/react';
 import type { GiscusProps as GiscusComponentProps } from '@giscus/react';
 import useTheme from '@/hooks/useTheme';
 import { BASE_URL } from '@/constants';
-export interface GiscusOptions extends Omit<GiscusComponentProps, 'id'> {}
 
-import LightTheme from './giscus-tailwind-light.css?url';
-import DarkTheme from './giscus-tailwind-dark.css?url';
+export interface GiscusOptions extends Omit<GiscusComponentProps, 'id'> {}
 
 export default function Giscus(props: GiscusOptions) {
   const { colorMode } = useTheme();
-  const baseUrl = window.location.origin + BASE_URL;
+
+  const themeUrl =
+    colorMode === 'dark'
+      ? `${BASE_URL}/assets/giscus-tailwind-dark.css`
+      : `${BASE_URL}/assets/giscus-tailwind-light.css`;
+
   return (
     <GiscusComponent
       id="comments"
-      // theme={colorMode === 'dark' ? new URL(DarkTheme, baseUrl).toString() : new URL(LightTheme, baseUrl).toString()}
-      // {...props}
-       repo="asadmash/asadsnote"
-        repoId="R_kgDOPOxLNA"
-        category="General"
-        categoryId="DIC_kwDOPOxLNM4CtJgq"
-        mapping="pathname"
-        strict="0"
-        reactionsEnabled="1"
-        emitMetadata="0"
-        inputPosition="bottom"
-        theme="preferred_color_scheme"
-        lang="en"
-       
+      repo="asadmash/asadsnote"
+      repoId="R_kgDOPOxLNA"
+      category="General"
+      categoryId="DIC_kwDOPOxLNM4CtJgq"
+      mapping="pathname"
+      strict="0"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="bottom"
+      theme={themeUrl}
+      lang="en"
     />
-  )
+  );
 }
